c672d2acc546153cbfba40c2ff97de91017ce83c
itsvinayyay <vinay.vashist2003@gmail.com>
2024-10-14 00:24:42 +0530

    Fixed Bugs

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             useMaterial3: true,
           ),
-          home: HomeScreen(),
+          home: const HomeScreen(),
         );
       },
     );
