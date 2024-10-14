fdc01f2548cf5898112db8cedb6b40f345b72c91
itsvinayyay <vinay.vashist2003@gmail.com>
2024-10-14 16:38:58 +0530

    Enabled IOS support

--- lib/core/routes/named_routes.dart
+++ lib/core/routes/named_routes.dart
@@ -1,7 +1,5 @@
 class Routes {
   static const String homeScreen = "./homeScreen";
   static const String formalInterviewScreen = "./formalInterviewScreen";
-  
-    static const String responseScreen = "./responseScreen";
-
+  static const String responseScreen = "./responseScreen";
 }
